f58a3dc8737cfe23dfd6767be649b53b53244f8c
szebestik@gmail.com <szebestik@gmail.com>
2022-07-10 12:46:11 +0200

    Added endpoint for publishable keys and client secret based on payment_intent

--- src/routes/stripe/stripe.js
+++ src/routes/stripe/stripe.js
@@ -4,16 +4,15 @@ router = express.Router();
 
 const stripe = require('../../stripe')
 
-const prisma = require('../../prismaClient')
+const prisma = require('../../prismaClient');
+const { hasUserValues, isLoggedInValidator } = require('../../middlewares/authorization');
 
 router.post('/', async (req, res) => {
     // #swagger.summary = 'Used for receiving information about stripe webhook events'
     console.log('Processing stripe request')
     const payload = req.body
     const sig = req.headers['stripe-signature']
-    const endpoint_secret = process.env.NODE_ENV === 'development' ? 
-        process.env.WEBHOOK_SECRET_LOCAL :
-        process.env.WEBHOOK_SECRET_PRODUCTION
+    const endpoint_secret = process.env.WEBHOOK_SECRET
 
     let event
     try {
@@ -71,7 +70,61 @@ router.post('/', async (req, res) => {
     }
     catch (err) {
         console.log(err)
-        res.sendStatus(500)
+        return res.sendStatus(500)
+    }
+})
+
+router.get('/clientSecret/:payment_intent', /*isLoggedInValidator, hasUserValues,*/ async (req, res) => {
+    // #swagger.summary = 'Used for retrieving the client secret of the client payment by providing a payment_intent'
+
+    /*  #swagger.parameters['authorization'] = {
+                in: 'header',
+                description: 'Access token',
+    } */
+
+    /*  #swagger.parameters['payment_intent'] = {
+                in: 'params',
+                description: 'The payment intent',
+    } */
+    const payment_intent = req.params.payment_intent
+
+    if (typeof payment_intent !== 'string') {
+        return res.sendStatus(400)
+    }
+    try {
+        const intentStoredInDB = await prisma.reservation.findFirst({
+            where: {
+                payment_intent: payment_intent,
+                payment_status: "paid"
+            }
+        })
+
+        if (intentStoredInDB) {
+            return res.sendStatus(409)
+        }
+
+        const paymentIntent = await stripe.paymentIntents.retrieve(payment_intent);
+
+        return res.json({
+            client_secret: paymentIntent.client_secret
+        }).status(200)
+    }
+    catch (err) {
+        console.log(err)
+        return res.sendStatus(500)
+    }
+})
+
+router.get('/publishableKey', async (_, res) => {
+    // #swagger.summary = 'Used for retrieving the publishable key of the stripe API'
+    try {
+        return res.json({
+            publishable_key: process.env.STRIPE_PUBLISHABLE_KEY
+        }).status(200)
+    }
+    catch (err) {
+        console.log(err)
+        return res.sendStatus(500)
     }
 })
 
